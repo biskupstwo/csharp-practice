2d563aa64bbb94a650f493d6a0ec1fb36241fe49
biskupstwo <jbiskupski0@outlook.com>
2025-08-15 14:31:26 +0200

    Add solution to Playlist - Inner Class exercise

--- csharp-practice/exercises/Playlist/Album.cs
+++ csharp-practice/exercises/Playlist/Album.cs
@@ -4,43 +4,60 @@ public class Album
 {
     private string _name;
     private string _artist;
-    private List<Song> _songs;
+    private SongList _songs;
 
+    public class SongList
+    {
+        private List<Song> _songList;
+
+        public SongList(List<Song> songList)
+        {
+            _songList = songList;
+        }
+        
+        public bool Add(string title, double duration)
+        {
+            if (FindSong(title) != null) return false;
+            Song song = new Song(title, duration);
+            _songList.Add(song);
+            return true;
+        }
+        
+        public Song? FindSong(string title)
+        {
+            Song? song = null;
+
+            for (int i = 0; i < _songList.Count; i++)
+            {
+                if (_songList[i].GetTitle() == title)
+                {
+                    song = _songList[i];
+                    break;
+                }
+            }
+
+            return song;
+        }
+
+        public Song? FindSong(int trackNumber) => trackNumber <= 0 || trackNumber > _songList.Count ? null : _songList[trackNumber-1];
+
+    }
     public Album(string name, string artist)
     {
         _name = name;
         _artist = artist;
-        _songs = new List<Song>();
+        _songs = new SongList(new List<Song>());
     }
 
-    public bool AddSong(string title, double duration)
-    {
-        Song song = new Song(title, duration);
-        _songs.Add(song);
-        return _songs.Contains(song);
-    }
-
-    public Song? FindSong(string title)
-    {
-        Song? song = null;
-
-        for (int i = 0; i < _songs.Count; i++)
-        {
-            if (_songs[i].GetTitle() == title)
-            {
-                song = _songs[i];
-                break;
-            }
-        }
-
-        return song;
-    }
-
+    public bool AddSong(string title, double duration) => _songs.Add(title, duration);
+    public Song? FindSong(string title) => _songs.FindSong(title);
+    public Song? FindSong(int trackNumber) => _songs.FindSong(trackNumber);
+    
+    
     public bool AddToPlayList(int trackNumber, LinkedList<Song> playlist)
     {
-        int index = trackNumber - 1;
-        if (index < 0 || index >= _songs.Count) return false;
-        Song song = _songs[index];
+        Song? song = FindSong(trackNumber);
+        if (song == null) return false;
         playlist.AddLast(song);
         return playlist.Contains(song);
     }
